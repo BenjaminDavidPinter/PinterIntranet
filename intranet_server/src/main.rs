133e279dd673bd01660fbfa247c310499bc41fe9
BenjaminPinter <Ben.david.pinter@icloud.com>
2024-12-27 00:15:56 -0500

    HostName Resolution

--- intranet_server/src/main.rs
+++ intranet_server/src/main.rs
@@ -1,10 +1,17 @@
 use actix_files as fs;
 use actix_web::{App, HttpServer};
+use gethostname::gethostname;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut host_name = gethostname().into_string().unwrap();
+
+    if !host_name.contains("local") {
+        host_name = format!("{}.local", host_name);
+    }
+
     HttpServer::new(|| App::new().service(fs::Files::new("/", "./static").index_file("index.html")))
-        .bind("127.0.0.1:12000")?
+        .bind((host_name, 12000))?
         .run()
         .await
 }
